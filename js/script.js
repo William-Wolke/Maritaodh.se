56646e63fecb119b030c1d60360ff69fa3e3a44f
Isadora-Galindo-Hohn <61157472+Isadora-Galindo-Hohn@users.noreply.github.com>
2020-06-08 23:43:02 +0200

    mer js 0_0

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@ function initMap() {
         document.getElementById('map'), {zoom: 4, center: uluru});
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
-  }
+}
 
   let hemKnapp = document.getElementById('hem');
   let omMigKnapp = document.getElementById('om-mig');
   let prislistaKnapp = document.getElementById('prislista');
   let galleriKnapp = document.getElementById('galleri');
   let kontaktKnapp = document.getElementById('kontakt');
+
+  let hem = document.getElementById('hem-section');
+  let omMig = document.getElementById('om-mig-sektion');
+  let prislista = document.getElementById('prislista-sektion');
+  let galleriSektion = document.getElementById('galleri-sektion');
+  let kontaktSektion = document.getElementById('kontakt-sektion');
 
   hemKnapp.addEventListener("click", kontrolleraHemKnapp);
   omMigKnapp.addEventListener("click", kontrolleraOmMigKnapp);
@@ -23,21 +29,41 @@ function initMap() {
 
   
 function kontrolleraHemKnapp() {
-    
+    hemSektion.style.display = "none";
+    omMigSektion.style.display = "none";
+    prislistaSektion.style.display = "none";
+    galleriSektion.style.display = "none";
+    kontaktSektion.style.display = "none";
 }
 
 function kontrolleraOmMigKnapp() {
-
+    hemSektion.style.display = "none";
+    omMigSektion.style.display = "block";
+    prislistaSektion.style.display = "block";
+    galleriSektion.style.display = "block";
+    kontaktSektion.style.display = "block";
 }
 
 function kontrolleraPrislistaKnapp() {
-
+    hemSektion.style.display = "none";
+    omMigSektion.style.display = "block";
+    prislistaSektion.style.display = "block";
+    galleriSektion.style.display = "block";
+    kontaktSektion.style.display = "block";
 }
 
 function kontrolleraGalleriKnapp() {
-
+    hemSektion.style.display = "none";
+    omMigSektion.style.display = "block";
+    prislistaSektion.style.display = "block";
+    galleriSektion.style.display = "block";
+    kontaktSektion.style.display = "block";
 }
 
 function kontrolleraKontaktKnapp() {
-
+    hemSektion.style.display = "none";
+    omMigSektion.style.display = "block";
+    prislistaSektion.style.display = "block";
+    galleriSektion.style.display = "block";
+    kontaktSektion.style.display = "block";
 }
