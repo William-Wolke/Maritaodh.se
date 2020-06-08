f77ce873527cc7c21242e6733e4b83f996baefa3
Isadora-Galindo-Hohn <61157472+Isadora-Galindo-Hohn@users.noreply.github.com>
2020-06-08 23:21:54 +0200

    börjat på js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,36 @@ function initMap() {
     // The marker, positioned at Uluru
     var marker = new google.maps.Marker({position: uluru, map: map});
   }
+
+  let hemKnapp = document.getElementById('hem');
+  let omMigKnapp = document.getElementById('om-mig');
+  let prislistaKnapp = document.getElementById('prislista');
+  let galleriKnapp = document.getElementById('galleri');
+  let kontaktKnapp = document.getElementById('kontakt');
+
+  hemKnapp.addEventListener("click", kontrolleraHemKnapp);
+  omMigKnapp.addEventListener("click", kontrolleraOmMigKnapp);
+  prislistaKnapp.addEventListener("click", kontrolleraPrislistaKnapp);
+  galleriKnapp.addEventListener("click", kontrolleraGalleriKnapp);
+  kontaktKnapp.addEventListener("click", kontrolleraKontaktKnapp);
+
+  
+function kontrolleraHemKnapp() {
+    
+}
+
+function kontrolleraOmMigKnapp() {
+
+}
+
+function kontrolleraPrislistaKnapp() {
+
+}
+
+function kontrolleraGalleriKnapp() {
+
+}
+
+function kontrolleraKontaktKnapp() {
+
+}
