35b818292a9e2b9a9842b236e34376a5223827e9
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2020-06-11 22:25:22 +0200

    nya bilder + features

--- js/script.js
+++ js/script.js
@@ -124,3 +124,38 @@ gallery.querySelectorAll('.gallery-item').forEach(function (item) {
 //Utan denna line funkar inte galleriet korrekt
 galleriKnapp.addEventListener("click", resizeAll);
 galleriKnappMobil.addEventListener("click", resizeAll);
+
+
+
+
+
+
+
+
+
+
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+let img = document.getElementById("gallery-item");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+
+img.addEventListener("click", display);
+
+function display () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
